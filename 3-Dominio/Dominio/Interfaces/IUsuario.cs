4f7a3e5ac77aedc5c8fbf820f2279aabd2e41294
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-11-06 16:19:58 -0300

    Implementação das Interfaces e do Serviço na camada Dominio

---
+++ 3-Dominio/Dominio/Interfaces/IUsuario.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Dominio.Interfaces
+{
+    public interface IUsuario
+    {
+        Task<bool> AdicionaUsuario(string email, string senha, int idade, string celular);
+    }
+}
---
+++ 3-Dominio/Dominio/Servicos/ServicoNoticia.cs
@@ -0,0 +1,52 @@
+﻿using Dominio.Interfaces;
+using Dominio.Interfaces.InterfaceServicos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dominio.Servicos
+{
+    public class ServicoNoticia : IServicoNoticia
+    {
+        private readonly INoticia _iNoticia;
+
+        public ServicoNoticia(INoticia iNoticia)
+        {
+            _iNoticia = iNoticia;
+        }
+
+        public async Task AdicionaNoticia(Noticia noticia)
+        {
+            var validarTitulo = noticia.ValidarPropriedadeString(noticia.Titulo, "Titulo");
+            var validarInformacao = noticia.ValidarPropriedadeString(noticia.Informacao, "Informação");
+
+            if (validarTitulo && validarInformacao)
+            {
+                noticia.DataAlteracao = DateTime.Now;
+                noticia.DataCadastro = DateTime.Now;
+                noticia.Ativo = true;
+                await _iNoticia.Adicionar(noticia);
+            }
+        }
+
+        public async Task AtualizaNoticia(Noticia noticia)
+        {
+            var validarTitulo = noticia.ValidarPropriedadeString(noticia.Titulo, "Titulo");
+            var validarInformacao = noticia.ValidarPropriedadeString(noticia.Informacao, "Informação");
+
+            if (validarTitulo && validarInformacao)
+            {
+                noticia.DataAlteracao = DateTime.Now;
+                noticia.DataCadastro = DateTime.Now;
+                noticia.Ativo = true;
+                await _iNoticia.Atualizar(noticia);
+            }
+        }
+
+        public async Task<List<Noticia>> ListarNoticiasAtivas()
+        {
+            return await _iNoticia.ListarNoticias(n => n.Ativo);
+        }
+    }
+}
