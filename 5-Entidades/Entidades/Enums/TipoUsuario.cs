42af6c8be910cb4c148415659e803f7e556a0ea8
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-11-06 15:47:42 -0300

    Criação da Estrutura do Projeto e das Entidades

---
+++ 5-Entidades/Entidades/Enums/TipoUsuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades.Enums
+{
+   public enum TipoUsuario
+    {
+        Administrador = 1,
+        Comum = 2
+    }
+}
